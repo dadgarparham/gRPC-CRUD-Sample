40210eea05c2c22f744fd7eb18287c8dfd00a96d
mtrxsrvr <parham.dadgar@outlook.com>
2026-06-07 15:06:58 +0330

    Add swagger and Profile multi-project Running configs to start Solution.

--- GrpcCrudSample.Api/Program.cs
+++ GrpcCrudSample.Api/Program.cs
@@ -5,8 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddGrpc();
 builder.Services.AddSingleton<IPersonRepository, InMemoryPersonRepository>();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseSwagger().UseSwaggerUI(c => c.DocumentTitle = "Parham");
+app.UseHsts();
+app.UseHttpsRedirection();
 
 app.MapGrpcService<PersonGrpcService>();
 app.MapGet("/", () => "This service uses gRPC. Use a gRPC client to communicate.");
